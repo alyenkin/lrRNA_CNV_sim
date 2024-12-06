b045b12aa4341d55b51115dc9959bad512823e26
alyenkin <ayenkin@mgh.harvard.edu>
2024-12-06 11:44:16 -0500

    Made additions

--- scripts/gen_sequences.py
+++ scripts/gen_sequences.py
@@ -44,22 +44,30 @@ dup_bps = {
 }
 
 if __name__ == '__main__':
-    ref_file = 'ref/hg38.fa'
+    ref_file = 'ref/hg38_chr7.fa'
     muc3a = import_gtf('ref/MUC3A.gtf')
     ref = pysam.FastaFile(ref_file)
 
     for k, v in dup_bps.items():
         dup = utils.duplication(muc3a, v[0], v[1])
-        generate_fastq_file(f'output/fastq/{k}.fastq', dup.getsequence(ref), 3)
+        generate_fastq_file(f'output/fastq/{k}.fastq', dup.getsequence(ref), 100)
 
-    # fastq_files = Path('output/fastq').glob('*.fastq')
-    # for fq in fastq_files:
-    #     output_sam = f'output/alignments/{fq.stem}.sam'
-    #     command = f"minimap2 -ax splice:hq -uf {ref_file} {str(fq)} > {output_sam}"
-    #     subprocess.run(command, shell=True)
-    #
-    #     command = f"samtools view -bS {output_sam} > {output_sam.replace('.sam', '.bam')}"
-    #     
+    fastq_files = Path('output/fastq').glob('*.fastq')
+    for fq in fastq_files:
+        output_sam = f'output/alignments/{fq.stem}.sam'
+        command = f"minimap2 -ax splice:hq -uf {ref_file} {str(fq)} > {output_sam}"
+        subprocess.run(command, shell=True)
+
+        command = f"samtools view -bS {output_sam} > {output_sam.replace('.sam', '.bam')}"
+        subprocess.run(command)
+
+    sqanti3_path = Path("~/programs/SQANTI/SQANTI3-5.2.2")
+
+    bam_files = Path('output/alignments').glob('*.bam')
+    for bam in bam_files:
+        command = f"python {sqanti3_path}/sqanti3_qc.py -a {bam} -o {bam.stem}"
+        subprocess.run(command, shell=True)
+        
 
 
 
